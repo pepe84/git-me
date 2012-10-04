1b4a2c463b1169b9dbbece656a2bef460f9de4af
Pepe (jljc) <pepe.84@gmail.com>
2012-10-04 19:55:49 +0200

    List user orgs and repos

--- src/app/events.js
+++ src/app/events.js
@@ -14,6 +14,28 @@ App.Events = (function(lng, app, undefined) {
         });
     });
     
+    // TODO Dynamic user param
+    $$('#goto-orgs').tap(function(e){
+        app.Services.github.Orgs.getOrgsByUser(this.innerHTML, function(resp){
+            lng.View.Template.List.create({
+                el: '#orgs-list',
+                template: 'org-item-tpl',
+                data: resp
+            });
+        });
+    });
+    
+    // TODO Dynamic user param
+    $$('#goto-repos').tap(function(e){
+        app.Services.github.Repos.getReposByUser(this.innerHTML, function(resp){
+            lng.View.Template.List.create({
+                el: '#repos-list',
+                template: 'repo-item-tpl',
+                data: resp
+            });
+        });
+    });
+    
     return {
 
     }
--- src/app/services.js
+++ src/app/services.js
@@ -18,8 +18,23 @@ App.Services = (function(lng, app, undefined) {
                 }
             },
             Orgs: {
-                getOrg: function(id) {
-                    
+                getOrgsByUser: function(username, cb) {
+                    if (mockup) {
+                        cb(App.Mockups.github.orgs);
+                        return;
+                    }
+                    // TODO fix Access-Control-Allow-Origin issue using JSONP
+                    $$.get(basePath + '/users/' + username + '/orgs', {'callback': '?'} , cb);
+                }
+            },
+            Repos: {
+                getReposByUser: function(username, cb) {
+                    if (mockup) {
+                        cb(App.Mockups.github.repos);
+                        return;
+                    }
+                    // TODO fix Access-Control-Allow-Origin issue using JSONP
+                    $$.get(basePath + '/users/' + username + '/repos', {'callback': '?'} , cb);
                 }
             }
         }
